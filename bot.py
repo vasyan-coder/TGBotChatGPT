a658772dfa8864448011bfa4a11e57a871a2f2fa
Vasyan <vasyan.coder@gmail.com>
2023-02-06 22:07:28 +0300

    feat: reg start user handler in bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,15 @@ from aiogram import Bot, Dispatcher
 
 from config_data.config import Config, load_config
 
-# from handlers.user_handlers import register_user_handlers
+from handlers.user_handlers import register_user_handlers
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
 
 
 # Функция для регистрации всех хэндлеров
-# def register_all_handlers(dp: Dispatcher) -> None:
-#     register_user_handlers(dp)
-#     register_other_handlers(dp)
+def register_all_handlers(dp: Dispatcher) -> None:
+    register_user_handlers(dp)
 
 
 # Функция конфигурирования и запуска бота
@@ -36,7 +35,7 @@ async def main() -> None:
     dp: Dispatcher = Dispatcher()
 
     # Регистрируем все хэндлеры
-    # register_all_handlers(dp)
+    register_all_handlers(dp)
 
     # Запускаем polling
     try:
