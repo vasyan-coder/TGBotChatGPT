af505c38bd2aab46380167c6ed76cfb6fa27e529
Vasyan <vasyan.coder@gmail.com>
2023-02-07 10:17:34 +0300

    feat: add process_other_text in handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,9 +1,9 @@
 from aiogram import Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ChatAction
 from aiogram.filters import Command
 
 from lexicon.lexicon_ru import LEXICON_RU
-# from services.services import get_bot_choice, get_winner
+from services.sevices import get_chatGPT_answer
 
 
 # Этот хэндлер срабатывает на команду /start
@@ -11,11 +11,12 @@ async def process_start_command(message: Message):
     await message.answer(text=LEXICON_RU['/start'])
 
 
-# Этот хэндлер срабатывает на команду /help
-async def process_help_command(message: Message):
-    await message.answer(text=LEXICON_RU['/help'])
+# Этот хэндлер срабатывает на любое слово
+async def process_other_text(message: Message):
+    await message.answer(text=get_chatGPT_answer(message.text), action=ChatAction.TYPING)
 
 
 # Функция для регистрации хэндлеров в диспетчере. Вызывается в исполняемом файле bot.py
 def register_user_handlers(dp: Dispatcher):
     dp.message.register(process_start_command, Command(commands=['start', 'help']))
+    dp.message.register(process_other_text)
